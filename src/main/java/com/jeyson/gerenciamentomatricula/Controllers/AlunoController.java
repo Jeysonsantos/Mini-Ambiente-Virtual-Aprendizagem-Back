bcfdd043a3a12e63209fb5b806783f2ccdb21d7d
Jeyson <54117481+Jeysonsantos@users.noreply.github.com>
2023-07-08 00:37:52 -0300

    Adicionado a busca de todos alunos no banco

--- src/main/java/com/jeyson/gerenciamentomatricula/Controllers/AlunoController.java
+++ src/main/java/com/jeyson/gerenciamentomatricula/Controllers/AlunoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +34,10 @@ public class AlunoController {
         Aluno alunoCriado = alunoService.createAluno(aluno);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(alunoCriado.getId_aluno()).toUri();
         return ResponseEntity.created(uri).body(alunoCriado);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Iterable<Aluno>> listAlunos() {
+        return ResponseEntity.ok().body(alunoService.getAllAlunos());
     }
 }
--- src/main/java/com/jeyson/gerenciamentomatricula/Services/AlunoService.java
+++ src/main/java/com/jeyson/gerenciamentomatricula/Services/AlunoService.java
@@ -1,5 +1,7 @@
 package com.jeyson.gerenciamentomatricula.Services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -42,6 +44,10 @@ public class AlunoService {
     @Transactional
     public String getNomeAluno(Long id) {
         return alunoRepository.findById(id).get().getNome();
+    }
+
+    public List<Aluno> getAllAlunos() {
+        return this.alunoRepository.findAll();
     }
 
 }
